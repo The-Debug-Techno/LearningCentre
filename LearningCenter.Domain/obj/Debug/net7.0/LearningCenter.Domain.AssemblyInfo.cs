40e67d91b51808bd2e8ef63687b80ab7bb93cdea
Botirjon <thedavlativich@gmail.com>
2023-11-22 19:19:12 +0500

    Code refactor

--- LearningCenter.Domain/obj/Debug/net7.0/LearningCenter.Domain.AssemblyInfo.cs
+++ LearningCenter.Domain/obj/Debug/net7.0/LearningCenter.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearningCenter.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fefa6be4a79869175f2f6506f174ecd9a7f3c0bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearningCenter.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearningCenter.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
