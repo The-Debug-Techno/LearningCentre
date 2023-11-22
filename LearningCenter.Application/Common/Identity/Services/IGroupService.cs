0f799d1974a05795e32f9b18edf623c245a657c7
Botirjon <thedavlativich@gmail.com>
2023-11-22 19:18:35 +0500

    Add group service

---
+++ LearningCenter.Application/Common/Identity/Services/IGroupService.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+
+namespace LearningCenter.Application.Common.Identity.Services;
+
+public interface IGroupService
+{
+    IQueryable<Group> Get(Expression<Func<Group, bool>>? predicate = default, bool asNoTracking = false);
+
+    ValueTask<Group?> GetByIdAsync(Guid id, bool  asNoTracking = false, CancellationToken cancellationToken = default);
+
+    ValueTask<IList<Group>> GetByIdsAsync(IEnumerable<Guid> ids, bool asNoTracking = false, CancellationToken cancellationToken = default);
+
+    ValueTask<Group> CreateAsync(Group group, bool saveChanges = true, CancellationToken cancellationToken = default);
+
+    ValueTask<Group> UpdateAsync(Group group, bool saveChanges = true, CancellationToken cancellationToken= default);
+
+    ValueTask<Group?> DeleteByIdAsync(Guid id, bool saveChanges = true, CancellationToken cancellationToken = default);
+
+    ValueTask<Group?> DeleteAsync(Group group, bool saveChanges = true, CancellationToken cancellationToken = default);
+}
